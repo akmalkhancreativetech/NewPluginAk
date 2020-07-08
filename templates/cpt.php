9a5eaa57d66f129ae41b2586c2d12c1dd6ef3350
Akmal Khan <akmalkhancreativetechsol@gmail.com>
2020-07-08 07:13:44 -0700

    Work From Office Part 26b - Modular Custom Post Types

--- templates/cpt.php
+++ templates/cpt.php
@@ -1,1 +1,12 @@
-<h1>New Plugin Ak Cpt</h1>
+<div class="wrap">
+    <h1>New Plugin Ak Cpt</h1>
+    <?php settings_errors(); ?>
+
+    <form action="options.php" method="post">
+        <?php
+            settings_fields('new_plugin_ak_cpt_settings');
+            do_settings_sections('new_plugin_ak_cpt');
+            submit_button();
+        ?>
+    </form>
+</div>
